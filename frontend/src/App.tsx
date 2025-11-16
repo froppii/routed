1ae19bfae2f5e0629cd8cfca0d63148ec49e61ba
froppii <darlenewu1310@gmail.com>
2025-11-16 22:57:47 +0800

    a

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,9 @@
-import { useState } from 'react'
 import Map from './Map';
 
 function App() {
-  const feedUrls = [
-    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace',
-    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm',
-    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g',
-    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-jz',
-    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw',
-    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l',
-    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs',
-    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-si',
-  ];
-
   return (
     <div>
-        <Map feedUrls={feedUrls} />
+        <Map />
     </div>
   )
 }
